32f32be2cdeb9eb4cb015b5f62562ffa46a0877e
TarekIdrees <tarekidrees5@gmail.com>
2021-01-23 21:41:36 +0200

    first commit

---
+++ LinearRegression_one_variable_Code.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#read data
+path='dataset.txt'
+data=pd.read_csv(path,names=['Population','Profit'])
+data.plot(kind='scatter',x='Population',y='Profit',figsize=(5,5))
+
+#adding a new column called ones before the data
+data.insert(0,'Ones',1)
+
+#seperate X (training data) from y (target variable )
+cols=data.shape[1]
+X=data.iloc[ : ,0:cols-1]
+y=data.iloc[ : ,cols-1:cols]
+
+#Convert from data frames to numpy matrices
+X=np.matrix(X.values)
+y=np.matrix(y.values)
+theta=np.matrix(np.array([0,0]))                
+
+#cost function
+def ComputeCost(X,y,theta):
+    z=np.power(((X*theta.T)-y),2)
+    return np.sum(z)/(2*len(X))
+
+#gradientDescent function
+def GradientDescent(X,y,theta,alpha,iters):
+    temp=np.matrix(np.zeros(theta.shape))
+    parameters=int(theta.ravel().shape[1])
+    cost= np.zeros(iters)
+    
+    for i in range(iters):
+        error=(X * theta.T)-y
+        for j in range(parameters):
+            term=np.multiply(error,X[:,j])
+            temp[0,j]=theta[0,j]-((alpha/len(X))*np.sum(term))
+        theta=temp
+        cost[i]=ComputeCost(X,y,theta)
+    return theta,cost
+
+#initialize variables for learning rate and iterations
+alpha=0.01
+iters=1000
+
+#preform gradientDescent to 'fit' the model parameters 
+g,cost=GradientDescent(X, y, theta, alpha, iters)
+
+#get best fit line 
+x=np.linspace(data.Population.min(),data.Population.max(),100)
+print(x)
+f=g[0,0]+(g[0,1] * x)
+
+#draw the line
+fig,ax=plt.subplots(figsize=(7,5))
+ax.plot(x,f,'r',label='Prediction')
+ax.scatter(data.Population,data.Profit,label='Trianing Data')
+ax.legend(loc=2)
+ax.set_xlabel('Population')
+ax.set_ylabel('Profit')
+ax.set_title('Predicted Profit vs. Population Size')
+
+#draw error graph
+fig,ax=plt.subplots(figsize=(7,5))
+ax.plot(np.arange(iters),cost,'r')
+ax.set_xlabel('Iteration')
+ax.set_ylabel('Cost')
+ax.set_title('Error vs Training Data')
+
